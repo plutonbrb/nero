902d33bd72e16f4abd0e582f8464b8b691d94dc6
Danny Navarro <j@dannynavarro.net>
2015-03-20 17:07:03 +0100

    Test `Param` example

---
+++ examples/Param.hs
@@ -0,0 +1,40 @@
+module Param where
+
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit ((@?=), testCase)
+import Nero
+
+app1 :: Request -> Maybe Response
+app1 request = request ^? query . ix "name" . traverse <&> \name ->
+    httpOk ("<h1>Hello " <> name <> "</h1>")
+
+app2 :: Request -> Response
+app2 request = request ^. query . ix "name" & \name ->
+    httpOk ("<h1>Hello " <> unwords name <> "</h1>")
+
+tests :: TestTree
+tests = testGroup "HTTP parameters"
+    [ testGroup "Single value"
+        [ testCase "1 value"
+              $ run1 (pure "there")
+            @?= Just (httpOk "<h1>Hello there</h1>")
+        , testCase "First one of 2 values"
+              $ run1 ["out", "there"]
+            @?= Just (httpOk "<h1>Hello out</h1>")
+        ]
+    , testGroup "Multiple values"
+        [ testCase "1 value"
+              $ run2 (pure "there")
+            @?= httpOk "<h1>Hello there</h1>"
+        , testCase "Concatenating 2 values"
+              $ run2 ["out", "there"]
+            @?= httpOk "<h1>Hello out there</h1>"
+        ]
+    ]
+  where
+    run a p = a $ dummyRequest & query . at "name" ?~ p
+    run1 = run app1
+    run2 = run app2
+
+main :: IO ()
+main = defaultMain tests
--- examples/test.hs
+++ examples/test.hs
@@ -4,9 +4,11 @@ import Test.Tasty
 
 import qualified Hello
 import qualified SlashRedirect
+import qualified Param
 
 main :: IO ()
 main = defaultMain $ testGroup "Examples"
      [ Hello.tests
      , SlashRedirect.tests
+     , Param.tests
      ]
