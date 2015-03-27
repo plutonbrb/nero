75cf3d1696cc4f2bc80b99c34fe93af6d4f0e6ac
Danny Navarro <j@dannynavarro.net>
2015-03-27 09:27:44 +0100

    Remove unused test runner

--- tests/Test/Nero/Request.hs
+++
@@ -1,6 +0,0 @@
-module Test.Nero.Request (tests) where
-
-import Test.Tasty (TestTree, testGroup)
-
-tests :: TestTree
-tests = testGroup "Request" []
--- tests/tasty.hs
+++
@@ -1,6 +0,0 @@
-module Main (main) where
-
-import Test.Tasty (defaultMain, testGroup)
-
-main :: IO ()
-main = defaultMain $ testGroup "Nero" []
