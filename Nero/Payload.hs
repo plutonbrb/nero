ac516748904d86294844e95551ef1216d7481792
Danny Navarro <j@dannynavarro.net>
2015-03-28 14:20:23 +0100

    Add body getter for `Payload` and `Response`
    200 OK Response implementation uses `Payload` instead of a raw
    `ByteString`

--- Nero/Payload.hs
+++ Nero/Payload.hs
@@ -3,7 +3,11 @@
 module Nero.Payload
   ( Form
   , Payload
+  , payloadText
+  , Encoding
+  , utf8Encoding
   , Payloaded(..)
+  , Body(..)
   , Formed(..)
   , _Form
   , dummyPayloadForm
@@ -16,12 +20,32 @@ import Nero.Param
 
 type Form = MultiMap
 
-data Payload = PayloadBinary ByteString
+data Payload = PayloadText Encoding ByteString
+             | PayloadBinary ByteString
              | PayloadForm Form
                deriving (Show,Eq)
 
+data Encoding = Utf8
+              | Unknown String
+                deriving (Show,Eq)
+
+utf8Encoding :: Encoding
+utf8Encoding = Utf8
+
+payloadText :: Encoding -> ByteString -> Payload
+payloadText = PayloadText
+
 class Payloaded a where
     payload :: Traversal' a Payload
+
+-- Can't be made a Lens easily beacause of putative parsing failues for 'Form'
+class Body a where
+    body :: a -> ByteString
+
+instance Body Payload where
+    body (PayloadText _ b)   = b
+    body (PayloadBinary b) = b
+    body (PayloadForm fo)  = formEncode fo
 
 -- * Form url encoded payload
 
@@ -30,6 +54,9 @@ class Formed a where
 
 instance Formed Payload where
     form = _Form
+
+formEncode :: Form -> ByteString
+formEncode = undefined
 
 _Form :: Prism' Payload Form
 _Form = prism' PayloadForm $ \case
--- Nero/Response.hs
+++ Nero/Response.hs
@@ -7,15 +7,17 @@ module Nero.Response
   ) where
 
 import Control.Applicative ((<$>), pure)
-import Data.Monoid ((<>))
+import Data.Monoid ((<>), mempty)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as B8
 import Data.Text (Text)
 import Control.Lens
 import Data.Text.Strict.Lens (utf8)
+
+import Nero.Payload
 import Nero.Url
 
-data Response = Ok ByteString
+data Response = Ok Payload
               | MovedPermanently Url
                 deriving (Show,Eq)
 
@@ -24,7 +26,11 @@ instance Urled Response where
     urled _ response = pure response
 
 ok :: Text -> Response
-ok = Ok . review utf8
+ok = Ok . payloadText utf8Encoding . review utf8
+
+instance Body Response where
+    body (Ok pl) = body pl
+    body (MovedPermanently _) = mempty
 
 movedPermanently :: Url -> Response
 movedPermanently = MovedPermanently
