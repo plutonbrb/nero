2c517dcfc8f93418f48a0acdf3d1b7f4bfc6b363
Danny Navarro <j@dannynavarro.net>
2015-04-17 12:57:19 +0200

    Add an example of nested routing

--- examples/Combined.hs
+++ examples/Combined.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Combined where
 
+import Control.Applicative
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit ((@?=), testCase)
 import Nero
 import Data.Text.Lazy (Text)
+import Control.Arrow
 
 name :: Request -> Maybe Text
 name = preview (_GET . path . prefixed "/hello/")
@@ -13,17 +15,20 @@ surname :: Request -> Maybe Text
 surname = preview (param "surname")
 
 app1 :: Request -> Maybe Response
-app1 = preview (_GET . path . prefixed "/hello/") <&>
-    fmap (\n -> ok $ "<h1>Hello " <> n <> "</h1>")
+app1 = name <&> fmap (\n -> ok $ "<h1>Hello " <> n <> "</h1>")
 
 app2 :: Request -> Maybe Response
-app2 = preview (param "surname") <&>
-    fmap (\s -> ok $ "<h1>Hello " <> s <> "</h1>")
+app2 = surname <&> fmap (\s -> ok $ "<h1>Hello " <> s <> "</h1>")
 
 app12 :: Request -> Maybe Response
-app12 request = respond <$> name request <*> surname request 
+app12 request = respond <$> name request <*> surname request
   where
     respond n s = ok $ "<h1>Hello " <> n <> " " <> s <> "</h1>"
+
+nested :: Request -> Maybe Response
+nested = runKleisli . unwrapArrow
+       $ WrapArrow (Kleisli (preview (prefixed "/name") >=> app1))
+     <|> WrapArrow (Kleisli (preview (prefixed "/surname") >=> app2))
 
 tests :: TestTree
 tests = testGroup "Query parameters and routing"
@@ -31,6 +36,15 @@ tests = testGroup "Query parameters and routing"
         $ app12 (dummyRequest & path .~ "/hello/out"
                               & query . at "surname" ?~ pure "there")
       @?= Just (ok "<h1>Hello out there</h1>")
+    , testGroup "nested"
+      [ testCase "first"
+          $ nested (dummyRequest & path .~ "/name/hello/there")
+        @?= Just (ok "<h1>Hello there</h1>")
+      , testCase "second"
+           $ nested (dummyRequest & path .~ "/surname"
+                                  & query . at "surname" ?~ pure "there")
+         @?= Just (ok "<h1>Hello there</h1>")
+      ]
     ]
 
 main :: IO ()
