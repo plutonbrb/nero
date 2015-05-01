9553f807d7f8dabcb6722cf305cfcd5fb16c3fe4
Danny Navarro <j@dannynavarro.net>
2015-05-01 11:49:48 +0200

    Move CPP code to `Nero.Compat`

--- Nero/Compat.hs
+++ Nero/Compat.hs
@@ -1,12 +1,21 @@
+{-# LANGUAGE CPP #-}
+#if MIN_VERSION_base(4,8,0)
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+#endif
+
 module Nero.Compat
   ( Alt(..)
   ) where
 
+#if MIN_VERSION_base(4,8,0)
+import Data.Monoid (Alt(Alt, getAlt))
+#else
 import Nero.Prelude
+
 newtype Alt f a = Alt { getAlt :: f a }
                   deriving (Functor, Applicative, Alternative)
 
 instance Alternative f => Monoid (Alt f a) where
     mempty = empty
     Alt x `mappend` Alt y = Alt (x <|> y)
+#endif
