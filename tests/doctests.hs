98282c4e5cac362aec7ff14ce22aafdccf654bd8
Danny Navarro <j@dannynavarro.net>
2015-08-07 13:54:05 +0200

    Support stack

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE CPP #-}
 module Main where
 
 import System.FilePath.Glob (glob)
 import Test.DocTest (doctest)
 
 main :: IO ()
+#if !MIN_VERSION_doctest(0,10,1)
 main = doctest . ([ "-idist/build/autogen"
                   , "-optP-include"
                   , "-optPdist/build/autogen/cabal_macros.h"
                   , "Nero.hs"
                   ] ++) =<< glob "Nero/**/*.hs"
+#else
+main = doctest . ("Nero.hs" :) =<< glob "Nero/**/*.hs"
+#endif
