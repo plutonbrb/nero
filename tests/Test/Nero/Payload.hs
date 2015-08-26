112c007adc27fd64b0dafef5000400463ae7a90f
Danny Navarro <j@dannynavarro.net>
2015-08-26 16:23:37 +0200

    Add test for `_Form` as Prism

--- tests/Test/Nero/Payload.hs
+++ tests/Test/Nero/Payload.hs
@@ -10,18 +10,23 @@ import Nero.Prelude
 import Control.Applicative (liftA3)
 import Test.Tasty (TestTree, TestName, testGroup)
 import Test.Tasty.SmallCheck
-import Test.Tasty.SmallCheck.Lens (testLens)
-import Test.SmallCheck.Series
+import Test.Tasty.SmallCheck.Lens (testLens, testPrism)
+import Test.SmallCheck.Series (Serial, CoSerial)
 import Test.SmallCheck.Series.Instances ()
 
 import Nero.Payload
 import Nero.Param
 
-instance Monad m => Serial m Payload
+instance (Serial m a) => Serial m (NonEmpty a)
+instance (CoSerial m a) => CoSerial m (NonEmpty a)
 
+instance Monad m => Serial m MultiMap
+instance Monad m => CoSerial m MultiMap
+instance Monad m => Serial m Payload
 instance Monad m => Serial m Encoding
 
 tests :: TestTree
 tests = testGroup "Payload"
-  [ testLens (body :: Lens' Payload Body)
+  [ testLens  (body :: Lens' Payload Body)
+  , testPrism _Form
   ]
