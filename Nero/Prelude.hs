802b3103614dd195d45300329f754dc5ba4f0ece
Danny Navarro <j@dannynavarro.net>
2015-08-29 14:27:29 +0200

    Add infix flipped `<*>`

--- Nero/Prelude.hs
+++ Nero/Prelude.hs
@@ -1,11 +1,11 @@
-{-# OPTIONS_HADDOCK ignore-exports #-}
 {-|
 
-This module is meant to be imported unqualified. It takes care of
-different versions of `base` and re-exports the following:
+This module is meant to be imported unqualified. It adds custom `Prelude`
+functions, takes care of different versions of `base` and re-exports the
+following:
 
 - /everything/ from "Control.Lens".
--
+
 - 'Applicative', 'Alternative', '<$>', '<*>', '<|>', 'pure', 'empty' from "Control.Applicative".
 
 - 'Foldable', 'fold', 'foldMap' form "Data.Foldable".
@@ -13,13 +13,17 @@ different versions of `base` and re-exports the following:
 - '>=>', '<=<' from "Control.Monad".
 
 - 'Monoid', , 'mappend', 'mempty' from "Data.Monoid".
--
+
 - 'Semigroup', '<>' from "Data.Semigroup".
--
+
 - 'NonEmpty', ':|' from "Data.List.NonEmpty".
 
 -}
-module Nero.Prelude (module X) where
+module Nero.Prelude
+  ( module X
+  -- * Custom functions
+  , (<*&>)
+  ) where
 
 import Control.Applicative as X (Applicative, Alternative, (<$>), (<*>), (<|>), pure, empty)
 import Data.Foldable as X (Foldable, fold, foldMap)
@@ -28,3 +32,16 @@ import Data.Monoid as X (Monoid, mappend, mempty)
 import Data.Semigroup as X (Semigroup((<>)))
 import Data.List.NonEmpty as X (NonEmpty((:|)))
 import Control.Lens as X
+
+import Control.Applicative ((<**>))
+
+infixr 0 <*&>
+
+-- | A variant of '<*>' with the arguments reversed. It differs from '<**>' in
+--   its infix precedence so that it's possible to write this without the need
+--   of parenthesis:
+--
+--   >>> Just 'c' <*&> Just 'b' <*&> Just 'a' <&> (,,)
+--   Just ('a','b','c')
+(<*&>) :: Applicative f => f a -> f (a -> b) -> f b
+(<*&>) = (<**>)
